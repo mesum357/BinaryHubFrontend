2707f187e9f9e97b3ec6dbfdbd9f05448180bd39
Mesum Abbas <i.mesumabbas#@gmail.com>
2026-04-12 04:00:31 +0500

    Updated Binary Hub New Admin Panel

--- models/TeamMember.js
+++ models/TeamMember.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const teamMemberSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   department: { type: String, required: true, trim: true },
-  image: { type: String, default: '' }
+  image: { type: String, default: '' },
+  linkedinUrl: { type: String, default: '', trim: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('TeamMember', teamMemberSchema);
